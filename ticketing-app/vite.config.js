db435c1306a28b4d7dd96119686c661d0ce80efa
Quadri <kazeemquadri75@gmail.com>
2024-12-15 09:40:23 +0000

    config

--- ticketing-app/vite.config.js
+++ ticketing-app/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Ensure the output directory is correct
+  },
+  server: {
+    open: true, // Opens the browser when the dev server starts
+  },
+});
+
+
