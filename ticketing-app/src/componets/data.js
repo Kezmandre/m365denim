796531b8aac3b46aa8cfdc051bb87c95bcb0fcdc
Quadri <kazeemquadri75@gmail.com>
2024-12-15 13:48:32 +0000

    data

--- ticketing-app/src/componets/data.js
+++ ticketing-app/src/componets/data.js
@@ -2,4 +2,8 @@ export const userDatabase = [
   { firstName: "John", lastName: "Doe", registered:"false"},
   { firstName: "Quadri", lastName: "Kazeem", registered:"false"},
   { firstName: "Temitope", lastName: "Macjob", registered:"false"},
+  { firstName: "ololade", lastName: "kolejo", registered:"false"},
+  { firstName: "adetope", lastName: "aderogba", registered:"false"},
+  { firstName: "joseph", lastName: "Adekuoroye", registered:"false"},
+  { firstName: "adebimpe", lastName: "adebomi", registered:"false"},
 ];
