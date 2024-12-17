b7e9c66f157dbabaa1a2357234d4050e063ed341
Quadri <kazeemquadri75@gmail.com>
2024-12-17 13:37:24 +0000

    test

--- ticketing-app/src/componets/RegistrationForm.jsx
+++ ticketing-app/src/componets/RegistrationForm.jsx
@@ -1,25 +1,33 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { userDatabase as initialUserDatabase } from "./data"; // Assuming you have user data
-import Party from "../images/party.jpeg";
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { userDatabase as initialUserDatabase } from './data'; // Assuming you have user data
+import Party from '../images/party.jpeg';
 
 const RegistrationForm = () => {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [errorMessage, setErrorMessage] = useState(""); // State for error messages
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
   const navigate = useNavigate();
 
   // Load the user database from localStorage or fallback to the initial data
-  let userDatabase = JSON.parse(localStorage.getItem("userDatabase")) || [...initialUserDatabase];
+  let userDatabase = JSON.parse(localStorage.getItem('userDatabase')) || [...initialUserDatabase];
+
+  useEffect(() => {
+    // Check for existing registration flag in local storage
+    const registrationFlag = localStorage.getItem('userRegistrationFlag');
+    if (registrationFlag) {
+      setErrorMessage('You have already registered.');
+    }
+  }, []);
 
   const handleRegistration = () => {
     // Clear any previous error messages
-    setErrorMessage("");
+    setErrorMessage('');
 
     // Check if both first and last names are provided
     if (!firstName.trim() || !lastName.trim()) {
-      setErrorMessage("Both First Name and Last Name are required.");
-      setTimeout(() => setErrorMessage(""), 5000);
+      setErrorMessage('Both First Name and Last Name are required.');
+      setTimeout(() => setErrorMessage(''), 5000);
       return;
     }
 
@@ -32,17 +40,15 @@ const RegistrationForm = () => {
 
     if (!matchedUser) {
       // If user not found, display error message
-      setErrorMessage("Name not found in the invitation list.");
-      setTimeout(() => setErrorMessage(""), 5000);
+      setErrorMessage('Name not found in the invitation list.');
+      setTimeout(() => setErrorMessage(''), 5000);
       return;
     }
 
-    // Check if user registered flag exists in localStorage (assuming it's set elsewhere)
-    const userRegistered = localStorage.getItem(`${matchedUser.firstName}-${matchedUser.lastName}-registered`);
-
-    if (userRegistered) {
-      setErrorMessage("You have already registered.");
-      setTimeout(() => setErrorMessage(""), 5000);
+    // Check if the user has already registered
+    if (matchedUser.registered) {
+      setErrorMessage('You have already registered.');
+      setTimeout(() => setErrorMessage(''), 5000);
       return;
     }
 
@@ -50,10 +56,10 @@ const RegistrationForm = () => {
     matchedUser.registered = true;
 
     // Persist the updated userDatabase to localStorage
-    localStorage.setItem("userDatabase", JSON.stringify(userDatabase));
+    localStorage.setItem('userDatabase', JSON.stringify(userDatabase));
 
-    // Set a flag in localStorage to indicate registration on this device
-    localStorage.setItem(`${matchedUser.firstName}-${matchedUser.lastName}-registered`, true);
+    // Set the registration flag in local storage
+    localStorage.setItem('userRegistrationFlag', 'true');
 
     const userDetails = {
       firstName: matchedUser.firstName,
@@ -62,79 +68,48 @@ const RegistrationForm = () => {
     };
 
     // Navigate to the QR code page with user details
-    navigate("/qrcode", { state: { userDetails } });
+    navigate('/qrcode', { state: { userDetails } });
   };
 
   return (
-    // Rest of the component code remains the same
     <div className="w-[400px] mx-auto my-4 overflow-hidden bg-white shadow-2xl rounded-lg">
-    <div className="w-full">
-      <img
-        src={Party}
-        alt="Party"
-        className="w-full h-[70vh] bg-cover bg-center rounded-t-lg"
-      />
-    </div>
-    <h2 className="p-2 text-xl sm:text-2xl font-semibold font-serif">
-      M365 Denim Party. Register Here
-    </h2>
-
-    <div>
-      <div className="relative mx-auto w-[95%] mt-2">
-        <input
-          type="text"
-          id="firstName"
-          value={firstName}
-          onChange={(e) => setFirstName(e.target.value)}
-          name="firstName"
-          className="border-1 peer block w-full appearance-none rounded-lg border border-gray-300 bg-transparent px-2.5 pb-2.5 pt-4 text-sm text-gray-900 focus:border-green-600 focus:outline-none focus:ring-0"
-          placeholder="John"
-          autoComplete="off"
+      <div className="w-full">
+        <img
+          src={Party}
+          alt="Party"
+          className="w-full h-[70vh] bg-cover bg-center rounded-t-lg"
         />
-        <label
-          htmlFor="firstName"
-          className="absolute top-2 left-1 z-10 origin-[0] -translate-y-4 scale-75 transform cursor-text select-none px-2 text-sm bg-white font-semibold text-gray-500 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-green-600"
-        >
-          Enter Your First Name
-        </label>
       </div>
+      <h2 className="p-2 text-xl sm:text-2xl font-semibold font-serif">
+        M365 Denim Party. Register Here
+      </h2>
 
-      <div className="relative mx-auto mt-2 w-[95%]">
-        <input
-          type="text"
-          id="lastName"
-          name="lastName"
-          value={lastName}
-          onChange={(e) => setLastName(e.target.value)}
-          className="border-1 peer block w-full appearance-none rounded-lg border border-gray-300 bg-transparent px-2.5 pb-2.5 pt-4 text-sm text-gray-900 focus:border-green-600 focus:outline-none focus:ring-0"
-          placeholder="Doe"
-          autoComplete="off"
-        />
-        <label
-          htmlFor="lastName"
-          className="absolute top-2 left-1 z-10 origin-[0] -translate-y-4 scale-75 transform cursor-text select-none px-2 text-sm bg-white font-semibold text-gray-500 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-green-600"
-        >
-          Enter Your Last Name
-        </label>
-      </div>
-
-      {/* Error message */}
-      {errorMessage && (
-        <div className="text-red-500 mt-1 text-center">
-          <p className="text-center italic text-[14px]">{errorMessage}</p>
+      <div>
+        <div className="relative mx-auto w-[95%] mt-2">
+          {/* ... (input field for first name) */}
         </div>
-      )}
 
-      <div className="w-[95%] mx-auto">
-        <button
-          onClick={handleRegistration}
-          className="p-2 my-4 w-full bg-black text-white font-semibold rounded-lg outline-none text-xl"
-        >
-          Register
-        </button>
+        <div className="relative mx-auto mt-2 w-[95%]">
+          {/* ... (input field for last name) */}
+        </div>
+
+        {/* Error message */}
+        {errorMessage && (
+          <div className="text-red-500 mt-1 text-center">
+            <p className="text-center italic text-[14px]">{errorMessage}</p>
+          </div>
+        )}
+
+        <div className="w-[95%] mx-auto">
+          <button
+            onClick={handleRegistration}
+            className="p-2 my-4 w-full bg-black text-white font-semibold rounded-lg outline-none text-xl"
+          >
+            Register
+          </button>
+        </div>
       </div>
     </div>
-  </div>
   );
 };
 
